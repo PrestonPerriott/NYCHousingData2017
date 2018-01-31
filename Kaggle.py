58c95fe668341743d040324727400ff5950e6ef1
Preston Perriott <perriott@Prestons-MacBook-Pro.local>
2018-01-31 16:09:32 -0500

    cleaning up Bar graph function

--- Kaggle.py
+++ Kaggle.py
@@ -17,7 +17,7 @@ import cv2
 #TODO README file
 #Its worth Command clicking through all of the functions you're not familiar with
 #TODO All the Exception Handling
-#TODO Some naming conventions
+#TODO Some naming conventions & inline docu
 
 
 class fileReader():
@@ -38,14 +38,17 @@ class fileReader():
         freqNth = list(range(len(freqIndex)))           #0......nth of how many objects we have in the list
         freqDict = {
              'frequency' : freq,
-             'index'     : freqIndex,
+             'index/labels'     : freqIndex,
              'values'    : freqValues,
              'zero-based': freqNth
         }                                               #TODO and ability to limit the size of the column
         return freqDict
 
-    def createBar(self): #TODO finish func
-        return
+    def createBar(self, frequencyDict,title):
+        plt.bar(frequencyDict['zero-based'], frequencyDict['values'])          #plotting the actual Bar
+        plt.xticks(frequencyDict['zero-based'], frequencyDict['index/labels']) #mapping the bars to the labels
+        plt.title(title)
+        return plt
 
     def createHisto(self, numOfPlots, data, labels, loc, title, alpha):
         #If num of plots is greater than two, we should assume data is an array of arrays [ [1,2,3,4], [17, 27, 67, 23] ]
@@ -98,11 +101,15 @@ if __name__ == '__main__':
     print zipCodeFrequencyDict['frequency']
     print zipCodeFrequencyDict['zero-based']
 
-    #Using our function to create a bar graph more easily
     plt.bar(zipCodeFrequencyDict['zero-based'], zipCodeFrequencyDict['values']) #Plot the bars
     plt.xticks(zipCodeFrequencyDict['zero-based'], zipCodeFrequencyDict['index']) #add the labels
     plt.title('Zip Code Pref')
-    plt.show()
+    #plt.show()
+
+    # Using our function to create a bar graph more easily
+    barGraph = newReader.createBar(zipCodeFrequencyDict)
+    barGraph.show()
+
 
 
     # newReader.printColumn('ADDRESS')                  #Class func to just pass column name to return whole column
